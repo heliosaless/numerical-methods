829b9072682b392ff6881de62fdf9d1f96dd96bb
HMSS <heliowp11@gmail.com>
2021-07-04 19:46:43 -0300

    wew3e2e
    2e2e2e

--- gauss-legendre/main.cpp
+++ gauss-legendre/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <math.h> 
+#define pi 	3.14159265358979323846
+
 using namespace std;
 
 /*
@@ -9,19 +11,45 @@ using namespace std;
 
 */
 
-double f(double x){
-	return pow(sin(2*x) + 4*x*x + 3*x, 2);
+double xExp(double s, double a, double b){
+	return ((a+b) + (b-a)*tanh(s))/2;
+}
+
+double xExpExp(double s, double a, double b){
+	return ((a+b) + (b-a)*tanh((pi*sinh(s))/2))/2;
+}
+
+double dxExp(double s, double a, double b){
+	return (b-a)/(2*pow(cosh(s), 2));
+}
+
+
+double dxExpExp(double s, double a, double b){
+	return ((b-a)*pi*cosh(s))/(4*pow(cosh((pi*sinh(s))/2),2));
+}
+
+double function(double x){
+	//return pow(sin(2*x) + 4*x*x + 3*x, 2);
+	return 1/sqrt(x);
+	//return pow(x,-0.66666666666);
+	//return 1/(sqrt(4-pow(x,2)));
+}
+
+double f(double x, int exp, double a, double b){
+	if(exp == 0) return function(x);
+	if(exp == 1) return function(xExp(x, a, b))*dxExp(x, a, b);
+	if(exp == 2) return function(xExpExp(x, a, b))*dxExpExp(x, a, b);
 }
 
 double x(double alpha, double xi, double xf){
 	return ((xi+xf)/2) + ((xf-xi)*alpha/2);
 }
 
-double gl2point(double xi, double xf){
+double gl2point(double xi, double xf, int exp, double a, double b){
 	double alpha1 = -0.57735026919;
 	double alpha2 = 0.57735026919;
-	double fx1 = f(x(alpha1, xi, xf));
-	double fx2 = f(x(alpha2, xi, xf));
+	double fx1 = f(x(alpha1, xi, xf), exp, a, b );
+	double fx2 = f(x(alpha2, xi, xf), exp, a, b );
 
 	double w1 = 1;
 	double w2 = 1;
@@ -33,13 +61,13 @@ double gl2point(double xi, double xf){
 	return ((xf-xi)/2) * i;
 }
 
-double gl3point(double xi, double xf){
+double gl3point(double xi, double xf, int exp, double a, double b){
 	double alpha1 = -0.77459666924;
 	double alpha2 = 0;
 	double alpha3 = 0.77459666924;
-	double fx1 = f(x(alpha1, xi, xf));
-	double fx2 = f(x(alpha2, xi, xf));
-	double fx3 = f(x(alpha3, xi, xf));
+	double fx1 = f(x(alpha1, xi, xf), exp, a, b );
+	double fx2 = f(x(alpha2, xi, xf), exp, a, b );
+	double fx3 = f(x(alpha3, xi, xf), exp, a, b );
 	double w1 = 0.55555555555;
 	double w2 = 0.88888888888;
 	double w3 = 0.55555555555;
@@ -53,15 +81,15 @@ double gl3point(double xi, double xf){
 	return ((xf-xi)/2) * i;
 }
 
-double gl4point(double xi, double xf){
+double gl4point(double xi, double xf, int exp, double a, double b){
 	double alpha1 = -0.86113631159;
 	double alpha2 = -0.33998104358;
 	double alpha3 = 0.33998104358;
 	double alpha4 = 0.86113631159;
-	double fx1 = f(x(alpha1, xi, xf));
-	double fx2 = f(x(alpha2, xi, xf));
-	double fx3 = f(x(alpha3, xi, xf));
-	double fx4 = f(x(alpha3, xi, xf));
+	double fx1 = f(x(alpha1, xi, xf), exp, a, b);
+	double fx2 = f(x(alpha2, xi, xf), exp, a, b);
+	double fx3 = f(x(alpha3, xi, xf), exp, a, b);
+	double fx4 = f(x(alpha3, xi, xf), exp, a, b);
 	double w1 = 0.3478548451374539;
 	double w2 = 0.652145;
 	double w3 = 0.652145;
@@ -77,13 +105,18 @@ double gl4point(double xi, double xf){
 	return ((xf-xi)/2) * i;
 }
 
-double integrate(double xi, double xf, int points){
-	if(points == 2) return gl2point(xi,xf);
-	if(points == 3) return gl3point(xi,xf);
-	if(points == 4) return gl4point(xi,xf);
+double integrate_(double xi, double xf, int points, int exp, double a, double b){
+	if(points == 2) return gl2point(xi,xf, exp, a, b);
+	if(points == 3) return gl3point(xi,xf, exp, a, b);
+	if(points == 4) return gl4point(xi,xf, exp, a, b);
 }
+
+struct info{
+	double it;
+	double value;
+};
  
-double run(int a, int b, double eplison, int points){
+struct info integrate(double a, double b, int points, double eplison, int exp=0, double a_ = 0, double b_ = 0){
 	double old_ = 0, new_ = 0, error = 10000;
 	int n = 0;
 
@@ -95,28 +128,74 @@ double run(int a, int b, double eplison, int points){
 		for (int k = 0; k < n; k++){
 			double xi = a + k*delx;
 			double xf = xi + delx;
-			new_ += integrate(xi, xf, points);
+			new_ += integrate_(xi, xf, points, exp, a_, b_);
 		}
 
 		if(old_ != 0) error = fabs(new_ - old_)/old_;
 		old_ = new_;
 	}
+	struct info r;
+	r.it = n;
+	r.value = new_; 
+	return r;
+}
 
-	cout <<"Usando " << points << " pontos: Temos " << n << " iteracoes e " << "obtemos valor final " << new_ << endl;
+
+struct info runExp(double a, double b,  int points, int exp, double eplison1, double eplison2){
+	double c = 1;
+	double old_ = 0, new_ = 0;
+	double error = 10000;
+	while(error >= eplison1){
+		new_ = integrate(-c, c, points,eplison2, exp, a, b).value;
+
+		c = c + 0.1;
+		if(old_ != 0) error = fabs(new_ - old_)/old_;
+		old_ = new_;
+	}
+
+	struct info r;
+	r.it = c;
+	r.value = new_; 
+	return r;
+}
+
+void print(double a, double b, int points, double eplison){
+	struct info r = integrate(a, b, points, eplison);
+	cout << "Integrate from " << a << " to " << b << " with " << points << " points: ";
+	cout << r.value << " with " << r.it << " iterations." << endl;
+}
+
+void printExp(double a, double b, int points, int exp, double eplison1, double eplison2){
+	struct info r = runExp(a, b, points, exp, eplison1, eplison2);
+	cout << "Integrate from " << a << " to " << b << " with " << points << " points, using";
+	if(exp == 1) cout << " Simple Exponential";
+	else{
+		if(exp == 2) cout << " Double Exponential";
+		else {cout << "\n\n\n Use print instead.! \n";}
+	} 
+	cout << " formula: " <<  r.value << " with c = " << r.it << endl;
 }
 
 int main(int argc, char const *argv[])
 {
-	int a = 0;
-	int b = 1;
 
-	//cout << integrate(a,b,2) << endl;
-	//cout << integrate(a,b,3) << endl;
-	//cout << integrate(a,b,4) << endl;
+	double a = 0;
+	double b = 1;
+	int points = 2;
+	double eplison1 = 10e-6;
+	double eplison2 = 10e-6;
+	int exp = 1;
 
-	run(a,b, 10e-6, 2);
-	run(a,b, 10e-6, 3);
-	run(a,b, 10e-6, 4);
+
+	print(a,b, points, eplison1);
+	print(a,b, points+1, eplison1);
+	print(a,b, points+2, eplison1);
+	printExp(a,b, points, exp, eplison1, eplison2);
+	printExp(a,b, points+1, exp, eplison1, eplison2);
+	printExp(a,b, points+2, exp, eplison1, eplison2);
+	printExp(a,b, points, exp+1, eplison1, eplison2);
+	printExp(a,b, points+1, exp+1, eplison1, eplison2);
+	printExp(a,b, points+2, exp+1, eplison1, eplison2);
 
 
 	return 0;
--- newton-cotes/main.cpp
+++ newton-cotes/main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 double function(double x){
 	return pow(sin(2*x) + 4*x*x + 3*x, 2);
+	//return 1/(sqrt(x));
 }
 
 double closed_first(double xi, double xf, double delx){
@@ -33,10 +34,10 @@ double closed_third(double xi, double xf, double delx){
 double closed_fourth(double xi, double xf, double delx){
 	double h = delx/4;
 
-	return h*( 896*function(xi)/120.0 - 1472*function(xi+h)/120.0
-			+ 2528*function(xi+2*h)/120.0 -832*function(xi+3*h)/360.0
-			+ 448*function(xi+4*h)/60.0  )  / 24.0;
-
+	//return h*( 896*function(xi)/120.0 - 1472*function(xi+h)/120.0
+	//		+ 2528*function(xi+2*h)/120.0 -832*function(xi+3*h)/360.0
+	//		+ 448*function(xi+4*h)/60.0  )  / 24.0;
+	return 2*h*(7*function(xi) + 32*function(xi+h)+ 12*function(xi+2*h)+ 32*function(xi+3*h)+ 7*function(xi+4*h))/45;
 }
 
 double open_first(double xi, double xf, double delx){
@@ -56,28 +57,35 @@ double open_third(double xi, double xf, double delx){
 
 double open_fourth(double xi, double xf, double delx){
 	double h = delx/6;
-	return h*( 9504*function(xi+h)/120.0 - 17853*function(xi+2*h)/120.0
-			+ 26961*function(xi+3*h)/120.0 -15693*function(xi+4*h)/360.0
-			+ 4734*function(xf-h)/60.0  )  / 24.0;
+	//return h*( 9504*function(xi+h)/120.0 - 17853*function(xi+2*h)/120.0
+			//+ 26961*function(xi+3*h)/120.0 -15693*function(xi+4*h)/360.0
+			//+ 4734*function(xf-h)/60.0  )  / 24.0;
+
+	return 6*h*(11*function(xi+h) - 14*function(xi+2*h) + 26*function(xi+3*h) - 14*function(xi+4*h) + 11*function(xi+5*h))/20;
 }
 
-double integrate(double xi, double xf, double delx, bool closed, int degree){
+double integrate_(double xi, double xf, double delx, bool closed, int points){
 	if(closed){
-		if(degree == 1) return closed_first(xi, xf, delx);
-		else if(degree == 2) return closed_second(xi, xf, delx);
-		else if(degree == 3) return closed_third(xi, xf, delx);
-		else if(degree == 4) return closed_fourth(xi, xf, delx);
+		if(points == 2) return closed_first(xi, xf, delx);
+		else if(points == 3) return closed_second(xi, xf, delx);
+		else if(points == 4) return closed_third(xi, xf, delx);
+		else if(points == 5) return closed_fourth(xi, xf, delx);
 		else {return 0.;}
 	}else{
-		if(degree == 1) return open_first(xi, xf, delx);
-		else if(degree == 2) return open_second(xi, xf, delx);
-		else if(degree == 3) return open_third(xi, xf, delx);
-		else if(degree == 4) return open_fourth(xi, xf, delx);
+		if(points == 2) return open_first(xi, xf, delx);
+		else if(points == 3) return open_second(xi, xf, delx);
+		else if(points == 4) return open_third(xi, xf, delx);
+		else if(points == 5) return open_fourth(xi, xf, delx);
 		else {return 0.;}
 	}
 }
+
+struct info{
+	double it;
+	double value;
+};
  
-double run(int a, int b, double eplison, bool closed, int degree){
+struct info integrate(double a, double b, double eplison, bool closed, int points){
 	double old_ = 0, new_ = 0, error = 10000;
 	int n = 0;
 
@@ -89,32 +97,44 @@ double run(int a, int b, double eplison, bool closed, int degree){
 		for (int k = 0; k < n; k++){
 			double xi = a + k*delx;
 			double xf = xi + delx;
-			new_ += integrate(xi, xf, delx, closed, degree);
+			new_ += integrate_(xi, xf, delx, closed, points);
 		}
 
 		if(old_ != 0) error = fabs(new_ - old_)/old_;
 		old_ = new_;
 	}
 
-	cout << "N: " << n << " value: " << new_ << endl;
+	struct info r;
+	r.it = n;
+	r.value = new_;
+	return r;
+}
+
+void print(double a, double b, int points, bool closed, double eplison){
+	struct info r = integrate(a, b, eplison, closed, points);
+	cout << "Integrate from " << a << " to " << b << " with " << points << " points, using";
+	if (closed) cout <<  " closed";
+	else cout << " open";
+	cout << " formula: " <<  r.value << " with " << r.it << " iterations." << endl;
 }
 
 int main(int argc, char const *argv[])
 {
 	bool closed = true;
-	int degree = 1;
-	int a = 0;
-	int b = 1;
-	double eplison = 0.000001;
+	int points = 2;
+	double a = 0;
+	double b = 1;
+	double eplison = 10e-6;
 
-	run(a, b, eplison, true, 1);
-	run(a, b, eplison, false, 1);
-	run(a, b, eplison, true, 2);
-	run(a, b, eplison, false, 2);
-	run(a, b, eplison, true, 3);
-	run(a, b, eplison, false, 3);
-	run(a, b, eplison, true, 4);
-	run(a, b, eplison, false, 4);
+	print(a,b, points, closed, eplison);
+	print(a,b, points, !closed, eplison);
+	print(a,b, points+1, closed, eplison);
+	print(a,b, points+1, !closed, eplison);
+	print(a,b, points+2, closed, eplison);
+	print(a,b, points+2, !closed, eplison);
+	print(a,b, points+3, closed, eplison);
+	print(a,b, points+3, !closed, eplison);
+
 
 	return 0;
 }
